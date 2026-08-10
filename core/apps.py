aea7ebe030941004051f0c6d7aa233f31ebe228d
jgoleta <jgoleta@gbox.adnu.edu.ph>
2026-08-10 13:46:54 +0800

    Restrict Google login to ADNU domain
    Enforce sign-in using official ADNU Google accounts by adding a signal that checks the social account email domain and aborts login with an error message if it doesn't match. Connect the signal in CoreConfig.ready. Update the login template to display Django messages. Add SOCIALACCOUNT_PROVIDERS Google config (scopes and 'hd' param) and set LOGIN_REDIRECT_URL. Also update requirements.txt to include python-decouple, django-allauth, requests, and PyJWT.

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        from allauth.socialaccount.signals import pre_social_login
+        from .signals import google_login_domain_check
+        pre_social_login.connect(google_login_domain_check)
