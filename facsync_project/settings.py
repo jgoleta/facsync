e0150c050fa59dcc469d15af0df2eb9afc11f542
jgoleta <jgoleta@gbox.adnu.edu.ph>
2026-08-31 15:54:11 +0800

    Make DEFAULT_FROM_EMAIL configurable

--- facsync_project/settings.py
+++ facsync_project/settings.py
@@ -178,7 +178,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 
 # Email. Production deployments can replace this with SMTP or a transactional
